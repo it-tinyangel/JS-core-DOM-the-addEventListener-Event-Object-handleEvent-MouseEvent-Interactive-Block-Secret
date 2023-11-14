5c0845e6abe86eaf32a0286f93b32cddf15d4a79
Iryna Romashko <137172921+it-tinyangel@users.noreply.github.com>
2023-11-14 21:47:29 +0200

    Second commit
    Separate functions have been created to change color, text, and event handlers

--- js/script.js
+++ js/script.js
@@ -1,42 +1,59 @@
-// Get an element from the DOM by its ID
+// Get elements from the DOM by their IDs
 const box = document.getElementById('box');
 const boxInfo = document.getElementById('boxInfo');
 
+// Event handler when hovering over a block
 box.addEventListener('mouseover', function () {
-changeBackgroundColor(box, 'khaki');
-changeTextColor(boxInfo, 'midnightblue');
-changeText(boxInfo, 'Do you want to know which one?');
+	applyHoverStyles();
 });
 
+// Event handler when exiting the block
 box.addEventListener('mouseout', function () {
-changeBackgroundColor(box, null);
-changeTextColor(boxInfo, null);
-changeText(boxInfo, 'I have a secret');
+	applyDefaultStyles();
 });
 
+// Event handler when a block is clicked
 box.addEventListener('mousedown', function () {
-changeBackgroundColor(box, 'black');
-changeTextColor(boxInfo, 'white');
-changeText(boxInfo, 'And I\'ll not say!');
+	applyClickStyles();
 });
 
+// Event handler after the click is released
 box.addEventListener('mouseup', function () {
-changeBackgroundColor(box, 'khaki');
-changeTextColor(boxInfo, 'cornflowerblue');
-changeText(boxInfo, 'Do you want to know which one?');
+	applyHoverStyles();
 });
 
-// Function to change the background color of the element
+// Function to apply default styles
+function applyDefaultStyles() {
+	changeBackgroundColor(box, null);
+	changeTextColor(boxInfo, null);
+	changeText(boxInfo, 'I have a secret');
+}
+
+// Function to apply styles on hover
+function applyHoverStyles() {
+	changeBackgroundColor(box, 'khaki');
+	changeTextColor(boxInfo, 'royalblue');
+	changeText(boxInfo, 'Do you want to know which one?');
+}
+
+// Function to apply styles on click
+function applyClickStyles() {
+	changeBackgroundColor(box, 'black');
+	changeTextColor(boxInfo, 'white');
+	changeText(boxInfo, 'And I\'ll not say!');
+}
+
+// Function to change the element's background color
 function changeBackgroundColor(element, color) {
-element.style.backgroundColor = color;
+	element.style.backgroundColor = color;
 }
 
 // Function to change the color of the element's text
 function changeTextColor(element, color) {
-element.style.color = color;
+	element.style.color = color;
 }
 
-// Function to change the text of the element
+// Function to change the element's text
 function changeText(element, text) {
-element.innerText = text;
+	element.innerText = text;
 }
